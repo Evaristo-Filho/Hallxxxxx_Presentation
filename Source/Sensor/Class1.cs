540011f5625bde26379523a253cd6c0c8cf9dbd9
Evaristo <evaristossfilho@hotmail.com>
2025-07-16 20:18:41 -0300

    non functional changes

---
+++ Source/Sensor/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensor
+{
+    public class Class1
+    {
+
+        public static int Main()
+        {
+            return 100;
+        }
+
+    }
+
+
+}
+
+
+public interface SensorOutput
+{
+    public void Current();
+    public void Resistence();
+    public void Frequency();
+    public void Voltage();
+
+}
